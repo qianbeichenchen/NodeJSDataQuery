9d4f8ca4d3ffd9565ba6d5660903fd55df9f8628
qianbiechenchen_dh@163.com <qianbiechenchen_dh@163.com>
2024-02-08 15:02:23 +0800

    数据源中新增了增删改接口

--- App.js
+++ App.js
@@ -9,40 +9,91 @@ app.use(cors());
 app.use(bodyParser.json())
 
 
-let list = [
-    {name: "Bert Garrett", age: 10, id: 1, address: 'beijing', sex: 0, class: 1},
-    {name: "Judy Gill", age: 33, id: 2, address: 'beijing', sex: 0, class: 1},
-    {name: "Paul Garner", age: 22, id: 3, address: 'beijing', sex: 0, class: 2}
+let studentList = [
+    {name: "张三", age: 7, id: 1, sex: 0, class: 1},
+    {name: "平平", age: 6, id: 2, sex: 0, class: 1},
+    {name: "果果", age: 8, id: 3, sex: 0, class: 2}
 ];
-
-app.get("/list", (req, res) => {
-    res.json(list);    //    将数据返回给服务端
+let classList = [
+    {id: 1, name: '一年级1班'},
+    {id: 2, name: '一年级2班'},
+    {id: 3, name: '二年级1班'},
+]
+/**
+ * studentList用来查询数据
+ * batchUpdate 接口批量处理
+ * add/delete/update 单个处理，如学生表修改了两条记录，会调两次update 新增了三条记录，会调三次insert接口，但是批量处理是发一次请求，传回数组项
+ */
+app.get("/studentList", (req, res) => {
+    res.json(studentList);
 });
 
-app.post('/update', (req, res) => {
-    // 得到post请求的数据
+app.post("/student/batchUpdate", (req, res) => {
     let obj = []
     const resultArray = req.body
     resultArray.forEach((item, index) => {
+        obj[index] = {
+            succeed: true
+        }
         if (item.type === 'update') {
             let _index = item.sourceIndex
-            list[_index] = item.dataItem
-
+            studentList[_index] = item.dataItem
         } else if (item.type === 'insert') {
-            list.push(item.dataItem)
-
+            studentList.push(item.dataItem)
+            obj[index].data = item.dataItem
         } else if (item.type === 'delete') {
             let _index = item.sourceIndex
-            list.splice(_index, 1)
-        }
-        obj[index] = {
-            succeed: true,
-            data: item.dataItem
+            studentList.splice(_index, 1)
         }
 
     })
     res.json(obj);
-})
+});
+app.post("/student/add", (req, res) => {
+    studentList.push(req.body)
+    res.json({succeed:true});
+});
+app.post("/student/update", (req, res) => {
+    studentList.forEach((item, index) => {
+        if (item.id == req.body.id) {
+            studentList[index] = req.body
+        }
+    })
+    res.json({succeed:true});
+});
+app.delete("/student/delete", (req, res) => {
+    studentList.forEach((item, index) => {
+        if (item.id == req.body[0].id) {
+            studentList.splice(index,1)
+        }
+    })
+    res.json({succeed:true});
+});
+
+app.get("/classList", (req, res) => {
+    res.json(classList);
+});
+app.post("/class/batchUpdate", (req, res) => {
+    let obj = []
+    const resultArray = req.body
+    resultArray.forEach((item, index) => {
+        obj[index] = {
+            succeed: true
+        }
+        if (item.type === 'update') {
+            let _index = item.sourceIndex
+            classList[_index] = item.dataItem
+        } else if (item.type === 'insert') {
+            classList.push(item.dataItem)
+            obj[index].data = item.dataItem
+        } else if (item.type === 'delete') {
+            let _index = item.sourceIndex
+            classList.splice(_index, 1)
+        }
+
+    })
+    res.json(obj);
+});
 
 app.listen("3000", () => {
     console.log("=========服务启动了，在3000端口=========");
